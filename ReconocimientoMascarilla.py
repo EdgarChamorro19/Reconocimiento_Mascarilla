4b53f83708c489f30fc6a3c1d4b0b716afb50ebd
Edgar <edfabrizio05@gmail.com>
2021-08-30 23:48:26 -0500

    Nos va a permitir reconocer si una persona tiene mascarilla

--- ReconocimientoMascarilla.py
+++ ReconocimientoMascarilla.py
@@ -2,7 +2,7 @@ import cv2
 import os
 import mediapipe as mp
 mp_face_detection = mp.solutions.face_detection
-LABELS = ["Con_mascarilla", "Sin_mascarilla"]
+LABELS = ["Con_Mascarilla", "Sin_Mascarilla"]
 # Leer el modelo
 face_mask = cv2.face.LBPHFaceRecognizer_create()
 face_mask.read("Facemodel.xml")
@@ -10,31 +10,31 @@ cap = cv2.VideoCapture(0)
 with mp_face_detection.FaceDetection(
      min_detection_confidence=0.5) as face_detection:
      while True:
-          ret, frame = cap.read()
+          ret, Imagen = cap.read()
           if ret == False: break
-          frame = cv2.flip(frame, 1)
-          height, width, _ = frame.shape
-          frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-          results = face_detection.process(frame_rgb)
-          if results.detections is not None:
-               for detection in results.detections:
+          Imagen = cv2.flip(Imagen, 1)
+          height, width, _ = Imagen.shape
+          Imagen_rgb = cv2.cvtColor(Imagen, cv2.COLOR_BGR2RGB)
+          resultado = face_detection.process(Imagen_rgb)
+          if resultado.detections is not None:
+               for detection in resultado.detections:
                     xmin = int(detection.location_data.relative_bounding_box.xmin * width)
                     ymin = int(detection.location_data.relative_bounding_box.ymin * height)
                     w = int(detection.location_data.relative_bounding_box.width * width)
                     h = int(detection.location_data.relative_bounding_box.height * height)
                     if xmin < 0 and ymin < 0:
                          continue
-                    face_image = frame[ymin : ymin + h, xmin : xmin + w]
-                    face_image = cv2.cvtColor(face_image, cv2.COLOR_BGR2GRAY)
-                    face_image = cv2.resize(face_image, (72, 72), interpolation=cv2.INTER_CUBIC)
+                    imagenCara = Imagen[ymin : ymin + h, xmin : xmin + w]
+                    imagenCara = cv2.cvtColor(imagenCara, cv2.COLOR_BGR2GRAY)
+                    imagenCara = cv2.resize(imagenCara, (72, 72), interpolation=cv2.INTER_CUBIC)
                     
-                    result = face_mask.predict(face_image)
-                    #cv2.putText(frame, "{}".format(result), (xmin, ymin - 5), 1, 1.3, (210, 124, 176), 1, cv2.LINE_AA)
+                    result = face_mask.predict(imagenCara)
+                    #cv2.putText(Imagen, "{}".format(result), (xmin, ymin - 5), 1, 1.3, (210, 124, 176), 1, cv2.LINE_AA)
                     if result[1] < 150:
-                         color = (0, 255, 0) if LABELS[result[0]] == "Con_mascarilla" else (0, 0, 255)
-                         cv2.putText(frame, "{}".format(LABELS[result[0]]), (xmin, ymin - 15), 2, 1, color, 2, cv2.LINE_AA)
-                         cv2.rectangle(frame, (xmin, ymin), (xmin + w, ymin + h), color, 4)
-          cv2.imshow("Frame", frame)
+                         color = (0, 255, 0) if LABELS[result[0]] == "Con_Mascarilla" else (0, 0, 255)
+                         cv2.putText(Imagen, "{}".format(LABELS[result[0]]), (xmin, ymin - 15), 2, 1, color, 2, cv2.LINE_AA)
+                         cv2.rectangle(Imagen, (xmin, ymin), (xmin + w, ymin + h), color, 4)
+          cv2.imshow("Imagen", Imagen)
           k = cv2.waitKey(30)
           if k == 27:
                break
